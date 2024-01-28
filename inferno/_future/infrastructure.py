c6642022a5f059512de6dc1faeb81949aac76050
Marissa Dominijanni <mdomini@buffalo.edu>
2024-01-27 22:45:44 -0500

    Added TensorList class to future.

--- inferno/_future/infrastructure.py
+++ inferno/_future/infrastructure.py
@@ -1,4 +1,9 @@
+from __future__ import annotations
+from collections.abc import Iterable
+from typing import Any
 from inferno import Hook
+from itertools import zip_longest
+import torch
 import torch.nn as nn
 
 
@@ -12,6 +17,7 @@ class HookBundle(Hook):
         hook (Hook): underlying hook.
         module (nn.Module): connected module.
     """
+
     def __init__(self, hook: Hook, module: nn.Module):
         # deregister hook
         if hook.registered:
@@ -42,3 +48,223 @@ class HookBundle(Hook):
         """Registers the hook as a forward hook/prehook."""
         if not self.registered:
             self.hook.register(self.module)
+
+
+class TensorList(nn.Module):
+    def __init__(self, length: int = 0):
+        nn.Module.__init__(self)
+        self.data = [None for _ in range(length)]
+
+    def __iter__(self):
+        return iter(self.data)
+
+    def __getitem__(
+        self, idx: int | slice
+    ) -> None | torch.Tensor | list[torch.Tensor | None]:
+        return self.data[idx]
+
+    def __setitem__(
+        self,
+        idx: int | slice,
+        value: None | torch.Tensor | Iterable[torch.Tensor | None],
+    ) -> None:
+        # singleton assignment
+        if value is None or isinstance(value, torch.Tensor):
+            # slice (wrap to prevent slicing tensor)
+            if isinstance(idx, slice):
+                self.data[idx] = [value]
+            # singleton
+            else:
+                self.data[idx] = value
+
+        # iterable assignment
+        else:
+            values = []
+
+            # test that assignment is iterable
+            try:
+                enum = enumerate(value)
+            except TypeError:
+                raise TypeError(
+                    f"'{type(value).__name__}' is not None, Tensor, or iterable."
+                )
+
+            # test that inner values are
+            for idx, v in enum:
+                if v is None or isinstance(v, torch.Tensor):
+                    values.append(v)
+                else:
+                    raise TypeError(
+                        f"value at position {idx} is neither None nor Tensor."
+                    )
+
+            # slice
+            if isinstance(idx, slice):
+                self.data[idx] = value
+            # singleton (slice to prevent inserting list)
+            else:
+                self.data[slice(idx)] = value
+
+    def __delitem__(self, idx: int | slice) -> None:
+        del self.data[idx]
+
+    def __iadd__(
+        self, value: None | torch.Tensor | Iterable[torch.Tensor | None]
+    ) -> None:
+        if value is None or isinstance(value, torch.Tensor):
+            self.append(value)
+        else:
+            self.extend(value)
+
+    @property
+    def size(self) -> int:
+        return len(self.data)
+
+    @size.setter
+    def size(self, value: int) -> None:
+        value = int(value)
+        if value < 0:
+            raise ValueError(f"'{type(self).__name__}.size' must be nonnegative.")
+        elif value > len(self.data):
+            self.data = [d for d, _ in zip_longest(self.data, range(value))]
+        else:
+            self.data = [d for d, _ in zip(self.data, range(value))]
+
+    def extra_repr(self):
+        lines, maxlen = [], len(str(len(self.data) - 1))
+
+        for idx, d in enumerate(self.data):
+            if isinstance(d, nn.Parameter):
+                shape = "×".join(str(s) for s in d.shape)
+                shape = shape if shape else "scalar"
+                device, dtype, grad = d.device, d.dtype, d.requires_grad
+                lines.append(
+                    f"{str(idx).rjust(maxlen)}: "
+                    f"Parameter( {shape}, {device}, {dtype}, grad={grad} )"
+                )
+            elif isinstance(d, torch.Tensor):
+                shape = "×".join(str(s) for s in d.shape)
+                shape = shape if shape else "scalar"
+                device, dtype = d.device, d.dtype
+                lines.append(
+                    f"{str(idx).rjust(maxlen)}: "
+                    f"Tensor( {shape}, {device}, {dtype} )"
+                )
+            elif d is None:
+                lines.append(f"{str(idx).rjust(maxlen)}: None")
+            else:
+                raise RuntimeError(
+                    f"'{type(self).__name__}' contains non-Tensor, non-None value."
+                )
+
+        return "\n".join(lines)
+
+    def get_extra_state(self) -> dict[str, Any]:
+        state = {"length": len(self.data), "data": {}, "meta": {}}
+        for idx, d in enumerate(self.data):
+            if d is not None:
+                state["data"][f"d_{idx}"] = d.detach()
+                if isinstance(d, nn.Parameter):
+                    state["meta"][f"d_{idx}"] = {
+                        "type": "parameter",
+                        "grad": d.requires_grad,
+                        "datagrad": d.data.requires_grad,
+                    }
+                else:
+                    state["meta"][f"d_{idx}"] = {
+                        "type": "tensor",
+                        "grad": d.requires_grad,
+                    }
+        return state
+
+    def set_extra_state(self, state: dict[str, Any]) -> None:
+        self.data = [None for _ in range(state["length"])]
+        for k, d in state["data"].items():
+            meta = state["meta"][k]
+            idx = int(k.rpartition("_")[-1])
+
+            match meta["type"]:
+                case "parameter":
+                    self.data[idx] = nn.Parameter(
+                        d.requires_grad_(meta["datagrad"]), meta["grad"]
+                    )
+
+                case "tensor":
+                    self.data[idx] = d.requires_grad_(meta["grad"])
+
+                case _:
+                    raise RuntimeError("recieved improperly formatted 'extra state'.")
+
+    def to(self, *args, **kwargs) -> TensorList:
+        self.datato(*args, **kwargs)
+        return nn.Module.to(*args, **kwargs)
+
+    def zero_grad(self, set_to_none: bool = True) -> None:
+        for d in self.data:
+            if isinstance(d, nn.Parameter):
+                if d.grad is not None:
+                    if set_to_none:
+                        d.grad = None
+                    else:
+                        if d.grad.grad_fn is not None:
+                            d.grad.detach_()
+                        else:
+                            d.grad.requires_grad_(False)
+                        d.grad.zero_()
+
+        return nn.Module.zero_grad(set_to_none=set_to_none)
+
+    def condense(self) -> None:
+        self.data_filter(lambda x: x is not None, exclude_none=False)
+
+    def data_filter_(
+        self, fn: callable[[torch.Tensor | None], bool], exclude_none: bool = True
+    ) -> None:
+        data = []
+        for d in self.data:
+            if d is not None or not exclude_none:
+                if fn(d):
+                    data.append(d)
+            else:
+                data.append(d)
+        self.data = data
+
+    def data_map_(
+        self,
+        fn: callable[[torch.Tensor | None], torch.Tensor | None],
+        exclude_none: bool = True,
+    ) -> None:
+        for idx in range(len(self.data)):
+            if self.data[idx] is not None or not exclude_none:
+                self.data[idx] = fn(self.data[idx])
+
+    def data_to(self, *args, **kwargs) -> None:
+        self.data_map_(lambda x: x.to(*args, **kwargs))
+
+    def append(self, value: torch.Tensor | None) -> None:
+        if value is None or isinstance(value, torch.Tensor):
+            self.data.append(value)
+        else:
+            raise TypeError(f"'{type(value).__name__}' is neither None nor Tensor.")
+
+    def extend(self, value: Iterable[torch.Tensor | None]) -> None:
+        # iterable cannot be tensor
+        if isinstance(value, torch.Tensor):
+            raise TypeError("extend 'value' cannot be a Tensor.")
+
+        values = []
+
+        # test that assignment is iterable
+        try:
+            enum = enumerate(value)
+        except TypeError:
+            raise TypeError(f"'{type(value).__name__}' is not iterable.")
+
+        # test that inner values are
+        for idx, v in enum:
+            if v is None or isinstance(v, torch.Tensor):
+                values.append(v)
+            else:
+                raise TypeError(f"value at position {idx} is neither None nor Tensor.")
+
+        self.data.extend(values)
