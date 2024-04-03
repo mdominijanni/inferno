40cc55ec04e87ffe70085be950d551164d2e038f
Marissa Dominijanni <mdomini@buffalo.edu>
2024-04-03 02:32:04 -0400

    LinearDense testing.

--- test/neural/test_connections.py
+++ test/neural/test_connections.py
@@ -1,3 +1,4 @@
+import math
 import pytest
 import random
 import torch
@@ -13,8 +14,11 @@ def randshape(mindims=1, maxdims=9, minsize=1, maxsize=9):
     )
 
 
-def aaeq(t0, t1, eps=1e-7) -> bool:
-    return torch.all((t0 - t1).abs() < eps)
+def aaeq(t0, t1, eps=1e-6) -> bool:
+    if torch.all(t0 == t1):
+        return True
+    else:
+        return torch.all((t0 - t1).abs() < eps)
 
 
 class TestLinearDense:
@@ -36,7 +40,7 @@ class TestLinearDense:
             in_shape,
             out_shape,
             step_time,
-            synapse=DeltaCurrent.partialconstructor(step_time, "previous", 1e-7),
+            synapse=DeltaCurrent.partialconstructor(1.0, "previous", 1e-7),
             bias=bias,
             delay=delay,
             batch_size=batch_size,
@@ -76,7 +80,7 @@ class TestLinearDense:
         inshape = randshape(1, 3, 2, 5)
         outshape = randshape(1, 3, 2, 5)
         batchsz = random.randint(1, 9)
-        delay = 5
+        delay = 4
         conn = self.makeconn(
             inshape,
             outshape,
@@ -84,19 +88,74 @@ class TestLinearDense:
             batchsz,
             delay=float(delay),
             weight_init=(lambda x: torch.rand_like(x)),
-            delay_init=(lambda x: torch.ones_like(x)),
+            delay_init=(lambda x, d=delay: torch.ones_like(x) * d),
         )
 
         self.load_synapse(conn, p=0.3)
         conn.synapse.spike_.align(0)
         inputs = conn.synapse.spike_.value.clone().detach()
-        for k in range(1, delay):
+        for k in range(1, delay + 1):
             outputs = conn(torch.zeros(batchsz, *inshape))
-            print(conn.synspike == inputs[..., 1])
-            assert False
             assert aaeq(
                 outputs,
                 torch.matmul(
                     inputs[..., k].view(batchsz, -1).float(), conn.weight.t()
                 ).view(batchsz, *outshape),
             )
+
+    def test_like_input(self):
+        inshape = randshape(1, 3, 2, 5)
+        outshape = randshape(1, 3, 2, 5)
+        batchsz = random.randint(1, 9)
+        conn = self.makeconn(
+            inshape,
+            outshape,
+            1.0,
+            batchsz,
+            weight_init=(lambda x: torch.rand_like(x)),
+        )
+
+        assert tuple(conn.like_input(conn.synapse.spike).shape) == (batchsz, *inshape)
+
+    def test_like_synaptic(self):
+        inshape = randshape(1, 3, 2, 5)
+        outshape = randshape(1, 3, 2, 5)
+        batchsz = random.randint(1, 9)
+        conn = self.makeconn(
+            inshape,
+            outshape,
+            1.0,
+            batchsz,
+            weight_init=(lambda x: torch.rand_like(x)),
+        )
+
+        assert (
+            tuple(conn.like_synaptic(torch.rand(batchsz, *inshape)).shape)
+            == conn.synapse.spike.shape
+        )
+
+    @pytest.mark.parametrize("hasoutdim", (True, False), ids=("outdim", "inonly"))
+    def test_presyn_receptive(self, hasoutdim):
+        inshape = randshape(1, 3, 2, 5)
+        outshape = randshape(1, 3, 2, 5)
+        batchsz = random.randint(1, 9)
+        conn = self.makeconn(
+            inshape,
+            outshape,
+            1.0,
+            batchsz,
+            weight_init=(lambda x: torch.rand_like(x)),
+        )
+        print(conn.synapse.spike.shape)
+        assert tuple(
+            conn.presyn_receptive(
+                torch.rand(batchsz, math.prod(inshape), math.prod(outshape))
+                if hasoutdim
+                else torch.rand(batchsz, math.prod(inshape))
+            ).shape
+        ) == (
+            batchsz,
+            math.prod(outshape) if hasoutdim else 1,
+            math.prod(inshape),
+            1,
+        )
